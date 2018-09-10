33b5ec47c413493a9d40c09b888f512917d78735
Jay Bates <podpersonspam@gmail.com>
2018-09-10 17:30:26 -0400

    fixed

--- initial-programs.sh
+++ initial-programs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo pacman -S firefox
 sudo pacman -S veracrypt
-sudo pacman -S  smplayer smplayer-themes smplayer-skins
+sudo pacman -S smplayer smplayer-themes smplayer-skins
 
 echo "################################################################"
 echo "##############    INITIAL PROGRAMS INSTALLED     ###############"
