72c61c0bf30e3d2f84a6c176a16206957146b1f7
Jay Bates <podpersonspam@gmail.com>
2018-09-10 16:33:00 -0400

    first commit

--- setup-git-v1.sh
+++ setup-git-v1.sh
@@ -9,7 +9,7 @@ set -e
 
 git init
 git config --global user.name "Jay Bates"
-git config --global user.email "podpersonspam@gmail.com"
+git config --global user.email "ectoplasmo@gmail.com"
 sudo git config --system core.editor nano
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=25000'
