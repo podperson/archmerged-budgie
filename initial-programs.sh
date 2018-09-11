5536fd0ad2d017723436ef45cc1267a4eae99130
podperson <podperson@vivaldi.net>
2018-09-11 13:58:29 -0400

    podbudgie1st

--- initial-programs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo pacman -S firefox
-sudo pacman -S veracrypt
-sudo pacman -S smplayer smplayer-themes smplayer-skins
-
-echo "################################################################"
-echo "##############    INITIAL PROGRAMS INSTALLED     ###############"
-echo "################################################################"
